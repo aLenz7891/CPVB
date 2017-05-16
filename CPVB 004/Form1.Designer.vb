e5f7fd99441379ed2696b0c121e5bc46e619fe94
Lenz <leiji@web.de>
2017-05-16 16:55:51 +0200

    Upload 001
    Basis-Projekt-Struktur

---
+++ CPVB 004/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Eingabe = New System.Windows.Forms.TextBox()
+        Me.Connect = New System.Windows.Forms.Button()
+        Me.ConnectEingabe = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(175, 250)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(85, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Suchen"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(12, 12)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(248, 187)
+        Me.ListView1.TabIndex = 1
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 203)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'Eingabe
+        '
+        Me.Eingabe.Location = New System.Drawing.Point(12, 250)
+        Me.Eingabe.Name = "Eingabe"
+        Me.Eingabe.Size = New System.Drawing.Size(154, 20)
+        Me.Eingabe.TabIndex = 3
+        '
+        'Connect
+        '
+        Me.Connect.Location = New System.Drawing.Point(175, 219)
+        Me.Connect.Name = "Connect"
+        Me.Connect.Size = New System.Drawing.Size(85, 23)
+        Me.Connect.TabIndex = 4
+        Me.Connect.Text = "Verbinden"
+        Me.Connect.UseVisualStyleBackColor = True
+        '
+        'ConnectEingabe
+        '
+        Me.ConnectEingabe.Location = New System.Drawing.Point(12, 219)
+        Me.ConnectEingabe.Name = "ConnectEingabe"
+        Me.ConnectEingabe.Size = New System.Drawing.Size(154, 20)
+        Me.ConnectEingabe.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(269, 285)
+        Me.Controls.Add(Me.ConnectEingabe)
+        Me.Controls.Add(Me.Connect)
+        Me.Controls.Add(Me.Eingabe)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Eingabe As TextBox
+    Friend WithEvents Connect As Button
+    Friend WithEvents ConnectEingabe As TextBox
+End Class
---
+++ CPVB 004/Form1.vb
@@ -0,0 +1,106 @@
+﻿Public Class Form1
+    ' Ziel ist die phonetische Dupletten-Suche in SQL
+    ' Start mit vorgefertigtem Suchen nach klarem Ergebnis
+    ' Vorgesetzt für Anfang
+    Public line As String
+    Public input As String
+    Public array As String() =
+        {"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"}
+    Sub Main()
+        input = Eingabe.Text
+        Dim Zahl As Int16 = 0
+        Dim Sicher As Int16 = 0
+        If input = String.Empty Then
+            WriteError()
+        Else
+            Dim result As String = Umwandeln(input)
+            Zahl = 0
+            Sicher = 0
+            WriteResult(input, result)
+            For Each item As String In array
+                Dim test As String = Umwandeln(item)
+                Zahl += 1
+                If test = result Then
+                    WriteResult(item, "")
+                    Sicher += 1
+                Else
+                    If Sicher = 0 And Zahl = array.Length Then
+                        WriteResult("", "")
+                    End If
+                End If
+            Next
+        End If
+    End Sub
+    Sub WriteError()
+        ListView1.Items.Add("Ein Fehler ist aufgetreten!")
+        Eingabe.Text = ""
+    End Sub
+    Sub WriteResult(args, args2)
+        ' Such-Ergebnis
+        If args Is String.Empty Then
+            ListView1.Items.Add("Leider keine Ergebnisse :( " & vbNewLine)
+        Else
+            ListView1.Items.Add(vbNewLine & "Ergebnis: " & args & "." & vbNewLine)
+            If args2 IsNot String.Empty Then
+                ListView1.Items.Add(vbNewLine & "Code: " & args2 & vbNewLine)
+            End If
+        End If
+    End Sub
+    Public Function Umwandeln(ByVal Word As String) As String
+        Return SoundEx(Word, 4)
+    End Function
+    Public Function SoundEx(ByVal Word As String, ByVal Length As Integer) As String
+        Dim Value As String = ""
+        Dim Size As Integer = Word.Length
+        If (Size > 1) Then
+            Word = Word.ToUpper()
+            Dim Chars() As Char = Word.ToCharArray()
+            Dim Buffer As New System.Text.StringBuilder
+            Buffer.Length = 0
+            Dim PrevCode As Integer = 0
+            Dim CurrCode As Integer = 0
+            Buffer.Append(Chars(0))
+            Dim i As Integer
+            Dim LoopLimit As Integer = Size - 1
+            For i = 1 To LoopLimit
+                Select Case Chars(i)
+                    Case "A", "Ä", "E", "I", "O", "Ö", "U", "Ü", "H", "W", "Y"
+                        CurrCode = 0
+                    Case "B", "F", "P", "V"
+                        CurrCode = 1
+                    Case "C", "G", "J", "K", "Q", "S", "X", "Z"
+                        CurrCode = 2
+                    Case "D", "T"
+                        CurrCode = 3
+                    Case "L"
+                        CurrCode = 4
+                    Case "M", "N"
+                        CurrCode = 5
+                    Case "R"
+                        CurrCode = 6
+                End Select
+                If (CurrCode <> PrevCode) Then
+                    If (CurrCode <> 0) Then
+                        Buffer.Append(CurrCode)
+                    End If
+                End If
+                If (Buffer.Length = Length) Then
+                    Exit For
+                End If
+            Next
+            Size = Buffer.Length
+            If (Size < Length) Then
+                Buffer.Append("0", (Length - Size))
+            End If
+            Value = Buffer.ToString()
+        End If
+        Return Value
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListView1.Items.Clear()
+        Main()
+    End Sub
+    Private Sub Connect_Click(sender As Object, e As EventArgs) Handles Connect.Click
+
+    End Sub
+End Class
